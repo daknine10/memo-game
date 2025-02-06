1d53eb1c6ccc491fed6e0286b8aefc180e82c99d
daknine10 <156335553+daknine10@users.noreply.github.com>
2025-02-06 13:02:24 +0000

    Fixed async function bug.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -11,24 +11,26 @@ export default function App() {
         const data = []
         async function getCats() {
             console.log('fetching cats')
-            for (let breed of breeds) {
+            const fetchPromises = breeds.map(breed => 
                 fetch(`https://api.api-ninjas.com/v1/cats?name=${breed}`, {
                     method: 'GET',
                     headers: {'X-Api-Key': 'XKc15D8DGgOdWjjwihSDwQ==kpEPznCfohq954yL'}
                 })
                 .then(response => response.json())
                 .then(response => data.push({name: response[0].name, url: response[0].image_link}))
-                .catch(error => console.log(error))         
-            }
-            setLoading(false)
-            setCards([data])
+                .catch(error => console.log(error))
+            );
+            Promise.all(fetchPromises).then(() => {
+                setLoading(false);
+                setCards(data);
+            });
         }
         getCats()
     }, [])
 
     return (
         <div>
-            {loading ? <Game cards={cards}/> : <div>Loading...</div>}
+            {loading ? <div>Loading...</div> : <Game cards={cards}/>}
         </div>
   )
 }
